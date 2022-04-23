e71a293b96c5c977890e8339706c3ca82361a2b9
scriptskali <104281088+scriptskali@users.noreply.github.com>
2022-04-23 18:36:45 -0400

    Update and rename README.md to DDBOT.py

---
+++ DDBOT.py
@@ -0,0 +1,21 @@
+
+import requests
+import os
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[39m'
+os.system("clear")
+print(+BLUE"▒█▀▀▄ ▒█▀▀▄ ▒█▀▀▀█ ▒█▀▀▀█ ░░ ▒█░░▒█ ▒█▀▀▀ ▒█▀▀█")
+print("     ▒█░▒█ ▒█░▒█ ▒█░░▒█ ░▀▀▀▄▄ ▀▀ ▒█▒█▒█ ▒█▀▀▀ ▒█▀▀▄")
+print("     ▒█▄▄▀ ▒█▄▄▀ ▒█▄▄▄█ ▒█▄▄▄█ ░░ ▒█▄▀▄█ ▒█▄▄▄ ▒█▄▄█")
+print("///////////////////ATTACK-WEB//////////////////")
+print("creador:s.c.r.i.p.t.s.k.a.l.i")
+print("tik tok:s.c.r.i.p.t.s.k.a.l.i")
+print("///////////////////ATTACK-DEMO/////////////////")
+print("en desarrollo")
